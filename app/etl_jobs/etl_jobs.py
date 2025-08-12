664e5eb9848fcb140a5838b13fee4b860996ecc1
HariKiran Reddy <HariKiran_Reddy@epam.com>
2025-08-12 21:57:53 +0530

    nifty_bulk_deals feature logic modified

--- app/etl_jobs/etl_jobs.py
+++ app/etl_jobs/etl_jobs.py
@@ -16,11 +16,12 @@ def daily_bulk_deals_update_transformation():
                                  df['Price'] * df['Quantity'],
                                  df['Price'] * df['Quantity'] * -1) / 10000000
 
-    result = round(df.groupby('Symbol', as_index=False)['Trade Value'].sum(), 2)
-    filtered = result[result['Trade Value'] >= 2]
+    net_quantity_value = round(df.groupby('Symbol', as_index=False)['Trade Value'].sum(), 2)
+    net_quantity_value.rename(columns={'Trade Value': 'Net Trade Value'}, inplace=True)
+    result = round(df.groupby(['Symbol', 'Client Name'], as_index=False)['Trade Value'].sum(), 2)
+    result = result.merge(net_quantity_value, on='Symbol')
+    result['Transaction'] = result['Trade Value'].apply(lambda x: 'Sold' if x <= 0 else 'Bought')
+    filtered = result[(result['Trade Value'] >= 2) | (result['Trade Value'] <= -2)]
     json_output = filtered.to_dict(orient='records')
     print(json_output)
     return json.dumps(json_output, indent=2)
-
-
-daily_bulk_deals_update_transformation()
